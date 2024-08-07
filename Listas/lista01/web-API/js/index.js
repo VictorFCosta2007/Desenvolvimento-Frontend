401bd32cd404eed3ba3a7307b645e02966c217e9
VictorFCosta2007 <victorferreiracosta@gmail.com>
2024-08-06 23:15:11 -0300

    Lista 1

---
+++ Listas/lista01/web-API/js/index.js
@@ -0,0 +1,52 @@
+let express = require('express')
+let app = express()
+const porta = 5000
+
+let produtos = [
+    {
+        nome: "Bolo de Morango",
+        valor: 29.99,
+        descricao: "Bolo refrescante de saboroso",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdocetier.com.br%2Fproduto%2Fbolo-ninho-com-morango%2F&psig=AOvVaw2Uw7pzK-clsYSiwyVAXPvH&ust=1722780277050000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCiiOn-2IcDFQAAAAAdAAAAABAQ"
+    },
+    {
+        nome: "Bolo de Chocolate",
+        valor: 49.99,
+        descricao: "Nada melhor do que comer um bolo docinho e geladinho pela manhã",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Freceitinhas.com.br%2Freceita%2Fbolo-de-chocolate-com-morango%2F&psig=AOvVaw2jG-yW193QanZ7BA7iaKoF&ust=1722780847071000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCEovaA2YcDFQAAAAAdAAAAABAE"
+    },
+    {
+        nome: "Bolo de Cenoura",
+        valor: 39.99,
+        descricao: "O bolo que tem sabor de lembranças...",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sabornamesa.com.br%2Fbolos%2Fbolo-de-cenoura-recheado&psig=AOvVaw3HLq3IF0iFfxkejcxI2FUN&ust=1722780891132000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLj9ocuB2YcDFQAAAAAdAAAAABAE"
+    },
+]
+
+app.get('/', (req, res) => {
+    try {
+        return res.json(produtos).status(200)
+    } catch(error) {
+        return res.json({message: "Operação invalida"}).status(400)
+    }
+})
+
+app.get('/produto', (req, res) => {
+    const nomeProduto = req.query.nome.toLowerCase;
+    const produtoEncontrado = produtos[nomeProduto];
+
+    if(produtoEncontrado) {
+        res.json(produtoEncontrado);
+    } else{
+        res.json({
+            nome: "Bolo de Morango",
+            valor: 29.99,
+            descricao: "Bolo refrescante de saboroso",
+            imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdocetier.com.br%2Fproduto%2Fbolo-ninho-com-morango%2F&psig=AOvVaw2Uw7pzK-clsYSiwyVAXPvH&ust=1722780277050000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCiiOn-2IcDFQAAAAAdAAAAABAQ"
+        });
+    }
+})
+
+app.listen(porta, () => {
+    console.log(`http://localhost:${porta}`);
+})
